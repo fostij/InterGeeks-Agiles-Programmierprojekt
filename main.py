22e579d03279801084f94fdcbadbb590a278f9f9
AntonDushchak <mrfreuden@gmail.com>
2026-06-20 19:46:24 +0200

    feat: add pipeline service for the launch point of the entire pipeline

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from src.models.multi_head_insurance.train import TrainConfig
 from src.ml_config import BEST_CHECKPOINT_PATH, INSURANCE_DATASET_PATH, MULTI_HEAD_DATASET_PATH, NETWORK_CONFIG_PATH, OUTPUT_CHECKPOINT_PATH
 from src.services.pipeline import PredictionResult, Pipeline
+
 def main() -> None:
     pipeline = Pipeline()
+    strings = ["Ich war gestern beim Supermarkt und wollte rückwärts ausparken. Hab mit meinem Golf komplett den Pfosten übersehen und ihn gerammt. Zum Glück nichts Wildes, nur ein fetter Kratzer an der Stoßstange. Trotzdem ärgerlich.",
+               "Im Berufsverkehr hat der Dreier-BMW vor mir plötzlich voll gebremst. Ich konnte mit meinem Fiesta nicht mehr rechtzeitig reagieren und bin ihm hinten draufgefahren. Ist aber nur ein Blechschaden – seine Stoßstange ist Schrott, bei mir ist kaum was zu sehen.",
+               "Ich habe heute Morgen beim Abbiegen die Kurve total geschnitten und bin mit dem Tesla voll gegen den Bordstein geknallt. Jetzt ist die Felge komplett zerkratzt und der Reifen hat ’ne Beule. Muss wohl in die Werkstatt.",
+               "Beim Einparken in der Innenstadt habe ich einen parkenden Benz touchiert. War super eng. Ich habe direkt gewartet, bis der Besitzer kam. Wir haben die Daten ausgetauscht. Kein Riesending, nur etwas Lack ab bei beiden.",
+               "Mir hat heute ein Audi die Vorfahrt genommen – klassisch Rechts vor Links missachtet. Ich habe noch versucht auszuweichen, aber es hat kurz gekracht. Mein Kotflügel ist jetzt eingedellt. Die Polizei war auch schon da."]
+   
+    for text in strings:
+        result = pipeline.run(text, None)
+        print(result)
 
-    result = pipeline.run("Der Audi A3 (1999) hatte einen Crash. Am abend bei klarem Wetter kam es dazu: ein anderes Fahrzeug traf die Seitenpartie bei dichtem Verkehr. Ergebnis: der Kühler ist beschädigt, das Fahrwerk ist beschädigt, Kratzer an der Fahrertür, die Karosseriegeometrie ist verzogen, linker Kotflügel eingedellt. Die Frontairbags wurden aktiviert. Der Vorfall ist bei der Verkehrspolizei registriert. Ein Abschleppdienst war erforderlich.")
-    print(result)
     return
 
 if __name__ == "__main__":
