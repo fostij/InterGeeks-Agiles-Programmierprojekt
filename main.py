31c55a8df64ec8a72e174f59553cfe231f3af55c
AntonDushchak <mrfreuden@gmail.com>
2026-06-21 20:44:08 +0200

    chore: delete old imports from requirements.txt

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from src.models.multi_head_insurance.train import TrainConfig
 from src.services.pipeline import PredictionResult, Pipeline
+from src.db.pipeline_repository import PipelineResultRepository
 
 def main() -> None:
+    repository = PipelineResultRepository()
+    p = Pipeline(repository)
+    result = p.run("Mit dem Subaru Legacy (1998) lief es heute leider schief. Zur hauptverkehrszeit bei klarem Wetter kam es zum Knall: das Fahrzeug fuhr in eine Straßenbegrenzung. Danach wurden diese Schaeden festgestellt: Kratzer an der Beifahrertür, vorderer Stoßfänger gebrochen. Es wurde keine Airbag-Ausloesung festgestellt.", 
+          "src/models/cnn/foto-DTP.jpg")
+    print(result)
     return
 
 if __name__ == "__main__":
