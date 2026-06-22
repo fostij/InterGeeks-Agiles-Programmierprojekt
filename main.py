579129b5dc208e138d31f40548d0d6bedf40edcb
Anton Dushchak <mrfreuden@gmail.com>
2026-06-22 11:43:22 +0200

    fix: inference none problem.

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from src.services.pipeline import PredictionResult, Pipeline
+from src.logging_config import setup_logging
 from src.db.pipeline_repository import PipelineResultRepository
 
 def main() -> None:
+    setup_logging()
     repository = PipelineResultRepository()
     p = Pipeline(repository)
     result = p.run("Mit dem Subaru Legacy (1998) lief es heute leider schief. Zur hauptverkehrszeit bei klarem Wetter kam es zum Knall: das Fahrzeug fuhr in eine Straßenbegrenzung. Danach wurden diese Schaeden festgestellt: Kratzer an der Beifahrertür, vorderer Stoßfänger gebrochen. Es wurde keine Airbag-Ausloesung festgestellt.", 
-          "src/models/cnn/foto-DTP.jpg")
+          )
     print(result)
     return
 
