94b955fa8a0be78275768c403fc2b103558f7711
AntonDushchak <mrfreuden@gmail.com>
2026-06-28 00:15:20 +0200

    feat: add pyproject.toml to avoid import conflicts when running a file from other roots

--- src/models/multi_head_insurance/multi_head_config.py
+++ src/models/multi_head_insurance/multi_head_config.py
@@ -3,12 +3,12 @@
 """
 
 # Trainingsparameter
-from ml_config import _BASE_DIR, MULTI_HEAD_MODEL_PATH
+from ml_config import MULTI_HEAD_MODEL_PATH
 
 
-BATCH_SIZE      = 4
+BATCH_SIZE      = 16
 LEARNING_RATE   = 2e-5
-EPOCHS          = 1
+EPOCHS          = 10
 MAX_GRAD_NORM   = 1.0
 WEIGHT_DECAY    = 0.01
 WARMUP_RATIO    = 0.1   # Anteil der Gesamtschritte für den Warmup (10%)
@@ -20,7 +20,7 @@ TEST_SPLIT      = 0.1
 SPLIT_SEED      = 42
 
 # Datengenerierung
-DATASET_COUNT   = 1000
+DATASET_COUNT   = 20000
 
 # Modell
 MODEL_NAME      = "uklfr/gottbert-base"
@@ -30,6 +30,6 @@ TRACKER_METRIC  = "partial_score"   # "partial_score" | "exact_match" | "val_los
 TRACKER_MODE    = "max"             # "max" für Accuracy-Metriken, "min" für Loss
 
 
-DEFAULT_DATASET_PATH        = str(_BASE_DIR / "data" / "processed" / "train_dataset.jsonl")
-DEFAULT_NETWORK_CONFIG_PATH = str(_BASE_DIR / "data" / "processed" / "network_config.json")
+DEFAULT_DATASET_PATH        = "data/processed/train_dataset.jsonl"
+DEFAULT_NETWORK_CONFIG_PATH = "data/processed/network_config.json"
 DEFAULT_CHECKPOINT_PATH     = str(MULTI_HEAD_MODEL_PATH)
